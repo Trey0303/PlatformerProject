5feaec8d81608368fee81127a3b0668c75ffb699
Trey0303 <61602725+Trey0303@users.noreply.github.com>
2023-07-21 12:36:52 -0700

    added ability to short jump

--- MyPlatformer/Assets/Scripts/PlayerMovement.cs
+++ MyPlatformer/Assets/Scripts/PlayerMovement.cs
@@ -9,7 +9,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float rotationSpeed = 750;
     [SerializeField]
-    private float jumpSpeed = 12;
+    private float jumpHeight = 2;
+
+    [SerializeField]
+    private float gravityMultiplier = 3f;
+
     [SerializeField]
     private float jumpButtonGracePeriod = .2f;
     [SerializeField]
@@ -19,7 +23,6 @@ public class PlayerMovement : MonoBehaviour
     private CharacterController characterController;//handles most of the difficult math problems for me...
     //private float originalstepOffset;//used for a bug that the character controller might have that causes weird collision when jumping into walls (bug might have been fixed)
     private float ySpeed;
-    private float gravityMultiplier = 3;
     private float? lastGroundedTime;//question mark means that this float can either be null or have an actual value
     private float? jumpButtonPressedTime;
 
@@ -90,8 +93,14 @@ public class PlayerMovement : MonoBehaviour
         movementDirection.Normalize();//helps prevent the player from moving faster(having a magnitude over 1) than intended when walking diagonally
 
         ////player jump
+        float gravity = Physics.gravity.y * gravityMultiplier;//controls how stong gravity is
 
-        ySpeed += Physics.gravity.y * Time.deltaTime * gravityMultiplier;//this add the players jump speed to gravity and also controls how stong gravity is
+        if(ySpeed > 0 && jumpControl.action.IsPressed() == false)//short jump
+        {
+            gravity *= 2;
+        }
+
+        ySpeed += gravity * Time.deltaTime ;//this add the players jump speed to gravity 
 
         if (characterController.isGrounded)
         {
@@ -109,24 +118,24 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
-        if (jumpControl.action.triggered)//jump input
+        if (jumpControl.action.IsPressed())//jump input
         {
             jumpButtonPressedTime = Time.time;
             
         }
 
-        if (Time.time - lastGroundedTime <= jumpButtonGracePeriod)
+        if (Time.time - lastGroundedTime <= jumpButtonGracePeriod)//checks if character has been grounded recently
         {
             ySpeed = -0.5f;//its set to -.5 b/c for some reason setting it to 0 makes the player stick to the ground and sometime wont jump when you tell it to
 
-            if (Time.time - jumpButtonPressedTime <= jumpButtonGracePeriod)
+            if (Time.time - jumpButtonPressedTime <= jumpButtonGracePeriod)//checks if jump button has been pressed recently
             {
                 //characterController.stepOffset = originalstepOffset;//for a unity controller bug
                 if(animator != null)
                 {
                     animator.SetTrigger("Jump");
                 }
-                ySpeed = jumpSpeed;
+                ySpeed = Mathf.Sqrt(jumpHeight * -3 * gravity);//this takes the height and gravity values and returns the required speed needed to reach the desired height
                 jumpButtonPressedTime = null;
                 lastGroundedTime = null;
             }
