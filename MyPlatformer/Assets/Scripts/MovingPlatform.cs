c1b8a8f5a9eec685d2859d4b169e8ff95b815c48
Trey0303 <61602725+Trey0303@users.noreply.github.com>
2023-07-24 11:50:52 -0700

    created a moving platform that the player can stand on but there is some weird scaling issue with the player b/c it has to be parented to the platform

---
+++ MyPlatformer/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private WaypointPath _waypointPath;
+
+    [SerializeField]
+    private float _speed;
+
+    private int _targetWaypointIndex;
+
+    private Transform _previousWaypoint;
+    private Transform _targetWaypoint;
+
+    private float _timeToWaypoint;//time it will take to get to the next waypoint
+    private float _elapsedTime;
+
+    private Vector3 originalPlayerScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TargetNextWaypoint();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _elapsedTime += Time.deltaTime;
+        float elapsedPercentage = _elapsedTime / _timeToWaypoint;//gets the percentage of the platforms journey to the next waypoint
+
+        elapsedPercentage = Mathf.SmoothStep(0, 1, elapsedPercentage);//slows down platform whenever it reaches next platform
+
+        transform.position = Vector3.Lerp(_previousWaypoint.position, _targetWaypoint.position, elapsedPercentage);//platform movement
+        transform.rotation = Quaternion.Lerp(_previousWaypoint.rotation, _targetWaypoint.rotation, elapsedPercentage);//rotates platform to match waypoints rotation
+
+        if (elapsedPercentage >= 1)//checks if platform has reached its target waypoint
+        {
+            TargetNextWaypoint();
+        }
+    }
+
+    private void TargetNextWaypoint()
+    {
+        _previousWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);//sets current waypoint as new previous waypoint
+        _targetWaypointIndex = _waypointPath.GetNextWaypointIndex(_targetWaypointIndex);//targets next waypoint
+        _targetWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);//sets current waupoint as next waypoint
+
+        _elapsedTime = 0;
+
+        float distanceToWaypoint = Vector3.Distance(_previousWaypoint.position, _targetWaypoint.position);//uses current waypoint and next waypoint to travel to in order to find the distance
+        _timeToWaypoint = distanceToWaypoint / _speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            originalPlayerScale = other.transform.localScale; // Store the player's original scale
+            other.transform.SetParent(transform);
+            Debug.Log("player on plaform");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.SetParent(null);
+            other.transform.localScale = originalPlayerScale; // Restore the player's original scale
+            Debug.Log("Player left platform");
+
+        }
+    }
+}
